b790eda7c4a28da64242d7e9f492a1699a37aef4
charlie.wang <charliewang324@gmail.com>
2022-06-05 03:18:13 +0800

    UPD: Clone graph

--- cloneGraph.js
+++ cloneGraph.js
@@ -50,4 +50,15 @@
 // Output: []
 // Explanation: This an empty graph, it does not have any nodes.
 
-const cloneGraph = (node) => {};
+const cloneGraph = (node) => {
+  const traverse = (node) => {
+    if (!map.has(node.val)) {
+      map.set(node.val, new Node(node.val));
+      map.get(node.val).neighbors = node.neighbors.map(traverse);
+    }
+    return map.get(node.val);
+  }
+  
+  let map = new Map();
+  return node ? traverse(node) : node;
+};
