42b68be59446a1db170255d5ee0f25884950fc31
charlie.wang <charliewang324@gmail.com>
2022-08-05 05:04:30 +0800

    ADD: Reverse linked list

---
+++ linkedList/reverseLinkedList.js
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/reverse-linked-list/
+
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+// ---------------------------------------------------------------------------
+// Example 1:
+
+// Input: head = [1,2,3,4,5]
+// Output: [5,4,3,2,1]
+
+// ---------------------------------------------------------------------------
+// Example 2:
+
+// Input: head = [1,2]
+// Output: [2,1]
+
+// ---------------------------------------------------------------------------
+// Example 3:
+
+// Input: head = []
+// Output: []
+
+const reverseList = (head) => {
+  let [prev, current] = [null, head];
+  while (current) {
+    [current.next, prev, current] = [prev, current, current.next];
+  }
+  return prev;
+};
