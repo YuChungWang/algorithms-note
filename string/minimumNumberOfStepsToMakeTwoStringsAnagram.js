9662ab60780746195aa47219c238705d4c9d7ffb
charlie.wang <charliewang324@gmail.com>
2022-10-09 16:19:04 +0800

    ADD: Minimum number of steps to make two strings anagram

---
+++ string/minimumNumberOfStepsToMakeTwoStringsAnagram.js
@@ -0,0 +1,49 @@
+// https://leetcode.com/problems/minimum-number-of-steps-to-make-two-strings-anagram/
+
+// You are given two strings of the same length s and t. In one step you can choose any character of t and replace it with
+// another character.
+
+// Return the minimum number of steps to make t an anagram of s.
+
+// An Anagram of a string is a string that contains the same characters with a different (or the same) ordering.
+
+// ---------------------------------------------------------------------------
+// Example 1:
+
+// Input: s = "bab", t = "aba"
+// Output: 1
+// Explanation: Replace the first 'a' in t with b, t = "bba" which is anagram of s.
+
+// ---------------------------------------------------------------------------
+// Example 2:
+
+// Input: s = "leetcode", t = "practice"
+// Output: 5
+// Explanation: Replace 'p', 'r', 'a', 'i' and 'c' from t with proper characters to make t anagram of s.
+
+// ---------------------------------------------------------------------------
+// Example 3:
+
+// Input: s = "anagram", t = "mangaar"
+// Output: 0
+// Explanation: "anagram" and "mangaar" are anagrams. 
+
+// Constraints:
+// 1 <= s.length <= 5 * 104
+// s.length == t.length
+// s and t consist of lowercase English letters only.
+
+const minSteps = (s, t) => {
+  let diffMap = new Map();
+  
+  for (let i = 0; i < s.length; i++) {
+    diffMap.set(s[i], (diffMap.get(s[i]) || 0) + 1);
+    diffMap.set(t[i], (diffMap.get(t[i]) || 0) - 1);
+  }
+  
+  const output = [...diffMap.values()].reduce((acc, curr) => {
+    return acc + Math.abs(curr);
+  }, 0);
+  
+  return output / 2;
+};
