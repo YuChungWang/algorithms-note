6a613ba6c22f854854d2d0438a1567beaf4cff41
charlie.wang <charliewang324@gmail.com>
2022-10-12 16:32:07 +0800

    ADD: Bubble sort

---
+++ sort/bubbleSort.js
@@ -0,0 +1,16 @@
+const arr = [8, 5, 7, 1, 9, 3];
+
+// Time complexity: O(n^2)
+const bubbleSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+
+  return arr;
+}
+
+console.log(bubbleSort(arr));
