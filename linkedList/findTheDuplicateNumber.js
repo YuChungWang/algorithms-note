dd84ce38eb6848e3726b257810e06c77bf0418a5
charlie.wang <charliewang324@gmail.com>
2023-03-11 02:06:24 +0800

    ADD: Find the duplicate number

---
+++ linkedList/findTheDuplicateNumber.js
@@ -0,0 +1,46 @@
+// https://leetcode.com/problems/find-the-duplicate-number/
+
+// Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+// There is only one repeated number in nums, return this repeated number.
+
+// You must solve the problem without modifying the array nums and uses only constant extra space.
+
+// ---------------------------------------------------------------------------
+// Example 1:
+
+// Input: nums = [1,3,4,2,2]
+// Output: 2
+
+// ---------------------------------------------------------------------------
+// Example 2:
+
+// Input: nums = [3,1,3,4,2]
+// Output: 3
+ 
+// ---------------------------------------------------------------------------
+// Constraints:
+
+// 1 <= n <= 105
+// nums.length == n + 1
+// 1 <= nums[i] <= n
+// All the integers in nums appear only once except for precisely one integer which appears two or more times.
+
+const findDuplicate = (nums) => {
+  let slow = nums[0];
+  let fast = nums[nums[0]];
+
+  while (slow !== fast) {
+    slow = nums[slow];
+    fast = nums[nums[fast]];
+  }
+
+  slow = 0;
+
+  while (slow !== fast) {
+    slow = nums[slow];
+    fast = nums[fast];
+  }
+
+  return slow;
+};
