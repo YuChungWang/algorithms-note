874af1c30f1810139ff7a5eae7d37cbc88570698
charlie.wang <charliewang324@gmail.com>
2023-06-13 20:37:15 +0800

    ADD: Binary tree postorder traversal

---
+++ tree/binaryTreePostorderTraversal.js
@@ -0,0 +1,45 @@
+// https://leetcode.com/problems/binary-tree-postorder-traversal/
+
+// Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+// ---------------------------------------------------------------------------
+// Example 1:
+
+// Input: root = [1,null,2,3]
+// Output: [3,2,1]
+
+// ---------------------------------------------------------------------------
+// Example 2:
+
+// Input: root = []
+// Output: []
+
+// ---------------------------------------------------------------------------
+// Example 3:
+
+// Input: root = [1]
+// Output: [1]
+
+// ---------------------------------------------------------------------------
+// Constraints:
+
+// The number of the nodes in the tree is in the range [0, 100].
+// -100 <= Node.val <= 100
+
+const postorderTraversal = (root) => {
+  let output = [];
+
+  const dfs = (root) => {
+    if (!root) {
+      return;
+    }
+
+    dfs(root.left);
+    dfs(root.right);
+    output.push(root.val);
+  };
+
+  dfs(root);
+
+  return output;
+};
